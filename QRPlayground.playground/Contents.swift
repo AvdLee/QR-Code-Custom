3a9bf809b0e7ab0a7bc99bb14951819003a60d8d
Antoine van der Lee <antoine@wetransfer.com>
2019-01-02 15:11:54 +0100

    Add Playground

---
+++ QRPlayground.playground/Contents.swift
@@ -0,0 +1,97 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+import CoreImage
+
+extension URL {
+
+    /// Creates a QR code for the current URL in the given color.
+    func qrImage(using color: UIColor, logo: UIImage? = nil) -> CIImage? {
+        let tintedQRImage = qrImage?.tinted(using: color)
+
+        guard let logo = logo?.cgImage else {
+            return tintedQRImage
+        }
+
+        return tintedQRImage?.combined(with: CIImage(cgImage: logo))
+    }
+
+    /// Returns a black and white QR code for this URL.
+    var qrImage: CIImage? {
+        guard let qrFilter = CIFilter(name: "CIQRCodeGenerator") else { return nil }
+        let qrData = absoluteString.data(using: String.Encoding.ascii)
+        qrFilter.setValue(qrData, forKey: "inputMessage")
+
+        let qrTransform = CGAffineTransform(scaleX: 12, y: 12)
+        return qrFilter.outputImage?.transformed(by: qrTransform)
+    }
+}
+
+extension CIImage {
+    /// Inverts the colors and creates a transparent image by converting the mask to alpha.
+    /// Input image should be black and white.
+    var transparent: CIImage? {
+        return inverted?.blackTransparent
+    }
+
+    /// Inverts the colors.
+    var inverted: CIImage? {
+        guard let invertedColorFilter = CIFilter(name: "CIColorInvert") else { return nil }
+
+        invertedColorFilter.setValue(self, forKey: "inputImage")
+        return invertedColorFilter.outputImage
+    }
+
+    /// Converts all black to transparent.
+    var blackTransparent: CIImage? {
+        guard let blackTransparentFilter = CIFilter(name: "CIMaskToAlpha") else { return nil }
+        blackTransparentFilter.setValue(self, forKey: "inputImage")
+        return blackTransparentFilter.outputImage
+    }
+
+    /// Applies the given color as a tint color.
+    func tinted(using color: UIColor) -> CIImage?
+    {
+        guard
+            let transparentQRImage = transparent,
+            let filter = CIFilter(name: "CIMultiplyCompositing"),
+            let colorFilter = CIFilter(name: "CIConstantColorGenerator") else { return nil }
+
+        let ciColor = CIColor(color: color)
+        colorFilter.setValue(ciColor, forKey: kCIInputColorKey)
+        let colorImage = colorFilter.outputImage
+
+        filter.setValue(colorImage, forKey: kCIInputImageKey)
+        filter.setValue(transparentQRImage, forKey: kCIInputBackgroundImageKey)
+
+        return filter.outputImage!
+    }
+}
+
+extension CIImage {
+
+    /// Combines the current image with the given image centered.
+    func combined(with image: CIImage) -> CIImage? {
+        guard let combinedFilter = CIFilter(name: "CISourceOverCompositing") else { return nil }
+        let centerTransform = CGAffineTransform(translationX: extent.midX - (image.extent.size.width / 2), y: extent.midY - (image.extent.size.height / 2))
+        combinedFilter.setValue(image.transformed(by: centerTransform), forKey: "inputImage")
+        combinedFilter.setValue(self, forKey: "inputBackgroundImage")
+        return combinedFilter.outputImage!
+    }
+}
+
+final class QRViewController : UIViewController {
+    let swiftLeeOrangeColor = UIColor(red:0.93, green:0.31, blue:0.23, alpha:1.00)
+    let swiftLeeLogo = UIImage(named: "swiftlee_qr_logo.png")!
+
+    override func loadView() {
+        guard let qrURLImage = URL(string: "https://www.avanderlee.com")?.qrImage(using: swiftLeeOrangeColor, logo: swiftLeeLogo) else { return }
+        let imageView = UIImageView(image: UIImage(ciImage: qrURLImage))
+        imageView.contentMode = .center
+        imageView.layer.backgroundColor = UIColor.white.cgColor
+        self.view = imageView
+    }
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = QRViewController()
